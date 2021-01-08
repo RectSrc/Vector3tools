552c8b546e7cd17119c33b8061f45b714cf67401
henry säker <khhs.xbox@gmail.com>
2021-01-08 16:09:13 +0100

    Yeet

--- vector3tools.cs
+++ vector3tools.cs
@@ -6,9 +6,31 @@ namespace vector3tools
     public static class vector3tools
     {
 
-        public static Vector3 Forward(Vector3 dir)
+        public class Vector3
         {
-            return new Vector3(dir.x * 1, dir.y * 0, dir.x * 1);
+
+            public float x;
+            public float y;
+            public float z;
+
+            public Vector3()
+            {
+                x = 0.0f;
+                y = 0.0f;
+                z = 0.0f;
+            }
+            public Vector3(float _x, float _y, float _z)
+            {
+                x = _x;
+                y = _y;
+                z = _z;
+            }
+
+        }
+
+        public static float[] Forward(float[] dir)
+        {
+            return new float[] { dir[0] * 1, dir[1] * 0, dir[2] * 1 };
         }
 
     }
