d4ea0ac538d1477c95875ba72946720829bc0ea9
henry säker <khhs.xbox@gmail.com>
2021-01-08 15:48:45 +0100

    Added some stuff

--- vector3tools.cs
+++ vector3tools.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Numerics;
+using System.Reflection;
 namespace vector3tools
 {
-    public class vector3tools
+    public static class vector3tools
     {
 
+        public void
 
     }
 }
