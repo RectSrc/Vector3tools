9171adc002c146304ce0d2a7db6417f92d4e6fbd
Khhs167 <34920681+Khhs167@users.noreply.github.com>
2021-01-09 16:51:50 +0100

    Update and rename vector3tools.cs to v3t.cs

--- v3t.cs
+++ v3t.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Numerics;
 using System.Reflection;
-namespace vector3tools
+namespace v3t
 {
-    public static class vector3tools
+    public static class v3t
     {
 
-        public class Vector3
+        public class v3t
         {
 
             public float x;
