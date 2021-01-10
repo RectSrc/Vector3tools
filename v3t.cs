c7131285b44dd9514faed0a13840e2c528c04f20
khhs <khhs.xbox@gmail.com>
2021-01-10 09:37:00 +0100

    Removed excess vecotr3 class

--- v3t.cs
+++ v3t.cs
@@ -39,26 +39,3 @@ namespace v3t
     }
 
 }
-
-
-public class Vector3
-{
-
-    public float x;
-    public float y;
-    public float z;
-
-    public Vector3()
-    {
-        x = 0.0f;
-        y = 0.0f;
-        z = 0.0f;
-    }
-    public Vector3(float _x, float _y, float _z)
-    {
-        x = _x;
-        y = _y;
-        z = _z;
-    }
-
-}
