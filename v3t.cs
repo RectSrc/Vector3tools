89a715f25a39f0dbdd41545384cd5e2ac0ea7381
khhs <khhs.xbox@gmail.com>
2021-01-11 11:11:22 +0100

    Fixed the entire Forward, yay!

--- v3t.cs
+++ v3t.cs
@@ -42,14 +42,19 @@ namespace v3t
             else
                 rot.x += Convert.ToSingle(Math.Round(Math.Sin(((Convert.ToSingle(Math.PI) * 2f) / 360f) * dir.y), 10));
             rot.z = Convert.ToSingle(Math.Round(Math.Sin(((Convert.ToSingle(Math.PI) * 2f) / 360f) * dir.y), 10));
-            
+            if (rot.z > 0 && Convert.ToSingle(Math.Round(Math.Cos(((Convert.ToSingle(Math.PI) * 2f) / 360f) * dir.z), 10)) > 0)
+                rot.z *= Convert.ToSingle(Math.Round(Math.Cos(((Convert.ToSingle(Math.PI) * 2f) / 360f) * dir.z), 10));
+            else
+                rot.z += Convert.ToSingle(Math.Round(Math.Cos(((Convert.ToSingle(Math.PI) * 2f) / 360f) * dir.z), 10));
+
+
             return rot;
         }
 
         public static void Main(string[] args)
         {
 
-            Console.WriteLine(Forward(new Vector3(90, 0, 0)).z);
+            Console.WriteLine(Forward(new Vector3(0, 90, 90)).z);
 
         }
 
